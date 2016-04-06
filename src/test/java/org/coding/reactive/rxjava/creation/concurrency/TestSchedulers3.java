0fcdff7b20d244497f0f69763afceee16aaa1be5
svante.kumlien <svante.kumlien@idealo.de>
2016-04-06 15:21:14 +0200

    add some placeholders for playing around with schedulers

--- src/test/java/org/coding/reactive/rxjava/creation/concurrency/TestSchedulers3.java
+++ src/test/java/org/coding/reactive/rxjava/creation/concurrency/TestSchedulers3.java
@@ -8,9 +8,9 @@ import org.junit.Test;
 public class TestSchedulers3 {
 
     @Test
-    public void testObservablesUsingOnSubscribeAndOnObserver() {
+    public void testOperatorWithOverloadedScheduler() {
         //Create an observable using an operator found here: http://reactivex.io/documentation/scheduler.html
-        //Specify schedulers using onSubscribe and onObserve.
+        //and use the overloaded method to provide your own scheduler.
         //Use one of the Utils. mehtods to debug the thread name
     }
 }
--- src/test/java/org/coding/reactive/rxjava/creation/concurrency/TestSchedulers4.java
+++ src/test/java/org/coding/reactive/rxjava/creation/concurrency/TestSchedulers4.java
@@ -8,9 +8,17 @@ import org.junit.Test;
 public class TestSchedulers4 {
 
     @Test
-    public void testDefaultSchedulerForOperators() {
+    public void testObservablesUsingOnSubscribeAndOnObserve() {
         //Create an observable using an operator found here: http://reactivex.io/documentation/scheduler.html
-        //and use the overloaded method to provide your own scheduler.
+        //Specify schedulers using onSubscribe and onObserve.
+        //Use one of the Utils. mehtods to debug the thread name
+    }
+
+
+    @Test
+    public void testObservablesUsingOnSubscribeAndMultipleOnObserve() {
+        //Create an observable using an operator found here: http://reactivex.io/documentation/scheduler.html
+        //Specify schedulers using onSubscribe and onObserve with different schedulers for different stages.
         //Use one of the Utils. mehtods to debug the thread name
     }
 }
