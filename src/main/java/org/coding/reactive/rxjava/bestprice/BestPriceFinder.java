2c7c8d322dd1595c07a04675785cb83295d148a0
Volker Sommerfeld <volker.sommerfeld@idealo.de>
2016-04-07 15:00:44 +0200

    - some first implementation of BestPricefinder [WIP]

--- src/main/java/org/coding/reactive/rxjava/bestprice/BestPriceFinder.java
+++ src/main/java/org/coding/reactive/rxjava/bestprice/BestPriceFinder.java
@@ -1,6 +1,7 @@
 package org.coding.reactive.rxjava.bestprice;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -15,6 +16,7 @@ import org.coding.reactive.rxjava.bestprice.model.Shop;
 import org.coding.reactive.rxjava.bestprice.service.DiscountService;
 import org.coding.reactive.rxjava.bestprice.service.ExchangeRateService;
 import org.coding.reactive.rxjava.bestprice.service.OfferService;
+import rx.Observable;
 
 public class BestPriceFinder {
 
@@ -23,9 +25,22 @@ public class BestPriceFinder {
     private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public Double getDiscountedPriceNullable(final Shop shop, final String product) throws InterruptedException, ExecutionException {
-        // TO BE IMPLEMENTED AS CODING DAY EXERCISE
-        // using getOfferNullableAsync(shop, product) and DiscountService::applyDiscount
-        return 0.0;
+        final Observable<Offer> offerObservable = Observable.just(OfferService.getOfferNullable(shop, product))
+                .filter(offer -> offer != null)
+                .map(DiscountService::applyDiscount);
+
+        return offerObservable.toBlocking().first().getPrice();
+//
+//
+//        final CompletableFuture<Offer> offerCompletableFuture = getOfferNullableAsync(shop, product)
+//                .thenApply(offer -> Optional.ofNullable(offer).map(DiscountService::applyDiscount).orElse(null));
+//
+//        return Optional.ofNullable(offerCompletableFuture.get()).map(Offer::getPrice).orElse(null);
+//
+//        shop.getDiscount()
+//        // TO BE IMPLEMENTED AS CODING DAY EXERCISE
+//        // using getOfferNullableAsync(shop, product) and DiscountService::applyDiscount
+//        return 0.0;
     }
 
     public Double getDiscountedPriceWithException(Shop shop, String product) throws InterruptedException, ExecutionException {
@@ -46,10 +61,10 @@ public class BestPriceFinder {
         return 0.0;
     }
 
-//    private CompletableFuture<Offer> getOfferNullableAsync(Shop shop, String product) {
-//        return CompletableFuture.supplyAsync(() -> OfferService.getOfferNullable(shop, product), executorService);
-//    }
-//
+    private CompletableFuture<Offer> getOfferNullableAsync(Shop shop, String product) {
+        return CompletableFuture.supplyAsync(() -> OfferService.getOfferNullable(shop, product), executorService);
+    }
+
 //    private CompletableFuture<Offer> getOfferWithExceptionAsync(Shop shop, String product) {
 //        return CompletableFuture.supplyAsync(() -> OfferService.getOfferWithException(shop, product), executorService);
 //    }
